85a776b37560a1d223bb54327a1a03c464587b88
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-04-05 16:01:23 -0600

    feat: rezize: input select y event change

--- public/JS/rezize.js
+++ public/JS/rezize.js
@@ -25,6 +25,9 @@ export function toggleRedimensionarListener(elemento) {
 
     elemento.style.width = width + 'px';
     elemento.style.height = height + 'px';
+
+    /** Actualizar valores */
+    actualizarValores(width, height);
   });
 
   document.addEventListener('mouseup', function () {
@@ -34,26 +37,51 @@ export function toggleRedimensionarListener(elemento) {
   });
 }
 
+function actualizarValores(width, height, UM = 'px') {
+  console.log('Actualizar valores');
+  FormWidth.ancho.value = width;
+  FormWidth.alto.value = height;
+  FormWidth.unidadAncho.value = UM;
+  FormWidth.unidadAlto.value = UM;
+}
+
 export function cambiarDimensiones(elemento) {
-  console.log(elemento);
   if (!FormWidth) return;
 
-  const anchoInput = FormWidth.ancho.value;
-  const altoInput = FormWidth.alto.value;
-
-  const unidadAnchoSelect = FormWidth.unidadAncho.value;
-  const unidadAltoSelect = FormWidth.unidadAlto.value;
-
   if (elemento) {
-    console.log(elemento.getBoundingClientRect());
     const rect = elemento.getBoundingClientRect();
     const ancho = rect.width;
     const alto = rect.height;
     console.log('Elemento: ancho', ancho, ' alto:', alto);
+    actualizarValores(ancho, alto);
   }
+
+  FormWidth.addEventListener('change', function (event) {
+    console.log(event.target.value);
+    const anchoInput = FormWidth.ancho.value;
+    const altoInput = FormWidth.alto.value;
+
+    const unidadAnchoSelect = FormWidth.unidadAncho.value;
+    const unidadAltoSelect = FormWidth.unidadAlto.value;
+
+    const ancho = parseInt(anchoInput);
+    const alto = parseInt(altoInput);
+    const unidadAncho = unidadAnchoSelect;
+    const unidadAlto = unidadAltoSelect;
+
+    if (elemento) {
+      elemento.style.width = ancho + unidadAncho;
+      elemento.style.height = alto + unidadAlto;
+    }
+  });
 
   FormWidth.addEventListener('submit', function (event) {
     event.preventDefault();
+    const anchoInput = FormWidth.ancho.value;
+    const altoInput = FormWidth.alto.value;
+
+    const unidadAnchoSelect = FormWidth.unidadAncho.value;
+    const unidadAltoSelect = FormWidth.unidadAlto.value;
 
     const ancho = parseInt(anchoInput);
     const alto = parseInt(altoInput);
@@ -63,9 +91,29 @@ export function cambiarDimensiones(elemento) {
     console.log('ancho:', ancho, 'UM:', unidadAncho);
     console.log('alto:', alto, 'UM:', unidadAlto);
 
-    // elemento.style.width = ancho + unidadAncho;
-    // elemento.style.height = alto + unidadAlto;
+    if (elemento) {
+      elemento.style.width = ancho + unidadAncho;
+      elemento.style.height = alto + unidadAlto;
+    }
   });
 }
 
 cambiarDimensiones();
+
+window.addEventListener('load', () => {
+  if (FormWidth) {
+    const anchoInput = FormWidth.ancho;
+    const altoInput = FormWidth.alto;
+
+    anchoInput.addEventListener('click', seleccionarContenido);
+    altoInput.addEventListener('click', seleccionarContenido);
+  }
+
+  function seleccionarContenido(e) {
+    const input = e.target;
+
+    if (input) {
+      input.select();
+    }
+  }
+});
