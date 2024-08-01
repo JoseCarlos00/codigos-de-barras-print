c4fa53671d2d057072610101b6edd11e2709b696
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-08-01 11:55:11 -0600

    feat: pnatillas.js: creacion de plantillas de codigos QR

---
+++ public/JS/plantillas.js
@@ -0,0 +1,72 @@
+async function plantillas() {
+  try {
+    const btnPlantilla = document.querySelectorAll('.plantillas .btn');
+
+    if (btnPlantilla.length === 0) {
+      throw new Error('No se enontraron botones de plantilla');
+    }
+
+    await setEventListener(btnPlantilla);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+
+  function setEventListener(btnPlantilla) {
+    return new Promise((resolve, reject) => {
+      btnPlantilla.forEach(btn => {
+        btn.addEventListener('click', setPlantilla);
+      });
+    });
+  }
+
+  function setPlantilla(e) {
+    console.log(e.target);
+    console.log(e.target.dataset.type);
+
+    const type = e.target.dataset.type;
+
+    if (type) {
+      insertPlantilla(type);
+    }
+  }
+
+  function getPlantilla(type = 'none') {
+    return new Promise((resolve, reject) => {
+      const platillaElementHTML = document.querySelector(
+        `section.plantillas > div.plantilla.${type}`
+      );
+
+      if (!platillaElementHTML) {
+        console.error('No se encontro la plantilla HTML');
+        reject();
+        return;
+      }
+
+      resolve(platillaElementHTML);
+    });
+  }
+
+  async function insertPlantilla(type) {
+    const areaDeImpresion = document.getElementById('areaDeImpresion');
+
+    if (!areaDeImpresion) {
+      console.error('No existe el area de impresion');
+      reject();
+      return;
+    }
+
+    // Obtén el elemento fuente y el elemento destino
+    const source = await getPlantilla(type);
+
+    // Vacía el contenido del elemento destino
+    areaDeImpresion.innerHTML = '';
+
+    // Clona todos los nodos hijos del elemento fuente (profundidad completa)
+    const clonedChildren = Array.from(source.childNodes).map(child => child.cloneNode(true));
+
+    // Añade cada nodo clonado al elemento destino
+    clonedChildren.forEach(child => areaDeImpresion.appendChild(child));
+  }
+}
+
+window.addEventListener('load', plantillas, { once: true });
