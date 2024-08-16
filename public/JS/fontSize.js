91a61e2fb89623f75edd418bc213ad6a5e1b94ce
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-08-16 15:09:19 -0600

    fix: editConten: se aagrego un solo evento submit y se agrego un evento de tecla ENTER

--- public/JS/fontSize.js
+++ public/JS/fontSize.js
@@ -7,20 +7,32 @@ function changeFontZize(elemento, size) {
 }
 
 if (FormFontSize) {
-  FormFontSize.fontSize.addEventListener('focus', function (e) {
-    e.target.select();
-  });
+  // FormFontSize.fontSize.addEventListener('focus', function (e) {
+  //   e.target.select();
+  // });
 
   FormFontSize.fontSize.addEventListener('input', function (e) {
     const newZize = this.value;
 
     if (elemento) {
       changeFontZize(elemento, newZize);
+      FormFontSize.fontSize2.value = newZize;
+    }
+  });
+
+  FormFontSize.fontSize2.addEventListener('input', function (e) {
+    const newZize = this.value;
+
+    if (elemento) {
+      changeFontZize(elemento, newZize);
+      FormFontSize.fontSize.value = newZize;
     }
   });
 }
 
 export function setElementoSize(elementoParams = null, set) {
+  console.log('[setElementoSize]:', elementoParams);
+
   if (set) {
     elemento = elementoParams;
   } else {
